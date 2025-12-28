3cae46ac8673ed2a25f9421052ca667661c8f791
ajones119 <agj1113@gmail.com>
2025-12-27 18:21:42 -0600

    fix floating image article

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://aramisjones.com',
   prefetch: true,
   vite: {
     plugins: [tailwindcss()]
