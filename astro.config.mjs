b05a6d1062e5f27ea84c0b67e4e7315478745500
ajones119 <agj1113@gmail.com>
2026-09-23 17:55:41 -0500

    fix production routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-
 import tailwindcss from '@tailwindcss/vite';
-import wasm from 'vite-plugin-wasm';
 
+import wasm from 'vite-plugin-wasm';
 import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aramisjones.com',
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: 'static',
   prefetch: true,
 
   vite: {
